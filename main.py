4aad44c3ec80b89c0de6ebdb3f47adc7204c0f6b
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-08-30 21:09:21 +0300

    aa

---
+++ main.py
@@ -0,0 +1,8 @@
+# Name: Xuan Dang
+
+# Phase 1:
+length = float(input("The length of the zander:"))
+if length >= 42:
+    print ("A zander fulfills the size limit")
+else:
+    print("The zander does not fulfill the size limit and release the fish back into the lake ")
