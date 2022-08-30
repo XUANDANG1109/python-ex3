8e2f02c5e7ef1c2616817a31144da7e0efb61196
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-08-30 22:56:23 +0300

    aa

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ if length >= 42:
     print ("A zander fulfills the size limit")
 else:
     print("The zander does not fulfill the size limit and release the fish back into the lake ")
+
+# Phase 2:
+cabin_class = input("The cabin class of a cruise ship:")
+if cabin_class == "LUX":
+    print("Upper-deck cabin with a balcony.")
+elif cabin_class == "A":
+    print("Above the car deck, equipped with a window.")
+elif cabin_class == "B":
+    print("Windowless cabin above the car deck.")
+elif cabin_class == "C":
+    print("Windowless cabin below the car deck.")
+else:
+    print("Invalid cabin class")
+
+# Phase 3:
+
